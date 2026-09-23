26b4451730b33ec59c1f2981b61326e81f557d39
Muhammad iman Hidayat <imanhidayatmuhammad@gmail.com>
2026-09-23 14:53:53 +0800

    Mengerjakan Latihan Mandiri: Kartu Mahasiswa

---
+++ p01-halo-java/src/main/java/id/ac/uniska/pbo2/p01/KartuMahasiswa.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package id.ac.uniska.pbo2.p01;
+
+/**
+ *
+ * @author ASUS
+ */
+public class KartuMahasiswa {
+    
+    public static void main(String[] args) {
+        // Deklarasi variabel dengan tipe data yang sesuai
+        String nama = "Muhammad Iman Hidayat";
+        String npm = "2410010023"; // Sesuaikan NPM Anda
+        String prodi = "Teknik Informatika";
+        int semester = 5;
+        String alasan = "Ingin memperdalam pemrograman berbasis objek dan pengembangan aplikasi";
+
+        // Menampilkan output sesuai format contoh
+        System.out.println("========================================");
+        System.out.println(" KARTU MAHASISWA PBO 2");
+        System.out.println("========================================");
+        System.out.println("Nama     : " + nama);
+        System.out.println("NPM      : " + npm);
+        System.out.println("Prodi    : " + prodi);
+        System.out.println("Semester : " + semester);
+        System.out.println("Alasan   : " + alasan);
+        System.out.println("========================================");
+    }
+}
+    
+
